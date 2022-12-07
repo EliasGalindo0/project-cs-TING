160d3a7365a8e0c26b2f91b858f474a3b70c2554
Elias <eliasgalindo@gmail.com>
2022-12-07 17:38:31 -0300

    fix: txt_file reader

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -6,10 +6,10 @@ def txt_importer(path_file):
         if not path_file.endswith('.txt'):
             print('Formato inválido', file=sys.stderr)
 
-        with open(path_file, encoding='utf8') as file:
-            data = list()
-        for row in file:
-            data.append(row.strip())
-            return data
+        with open(path_file) as file:
+            data = file.read()
+            txt_file = [text_line for text_line in data.split('\n')]
+            print(txt_file)
+            return txt_file
     except FileNotFoundError:
         print(f'Arquivo {path_file} não encontrado', file=sys.stderr)
