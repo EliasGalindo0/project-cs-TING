12f33ede4770d138bc0fc5f415cf30a876388f32
Elias <eliasgalindo@gmail.com>
2022-12-07 17:24:36 -0300

    fix lint

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -18,6 +18,6 @@ class Queue:
 
     def search(self, index):
         if 0 <= index < len(self._data):
-          return self._data[index]
+            return self._data[index]
         else:
-          raise IndexError
+            raise IndexError
