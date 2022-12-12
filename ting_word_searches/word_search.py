7ed7407299e2fe5e87ee7a4ace1fe035b9da2012
Elias Galindo <eliasgalindo@gmail.com>
2022-12-12 18:35:21 -0300

    feat: search by word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,39 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+    for index in range(len(instance)):
+        data = instance.search(index)
+        lines = data["linhas_do_arquivo"]
+        occur = []
+        for index in range(len(lines)):
+            if word.lower() in lines[index].lower():
+                occur.append(index + 1)
+        if len(occur):
+            result.append({
+                "palavra": word,
+                "arquivo": data["nome_do_arquivo"],
+                "ocorrencias": [{"linha": line} for line in occur]
+            })
+
+    return result
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+    for index in range(len(instance)):
+        data = instance.search(index)
+        lines = data["linhas_do_arquivo"]
+        occur = []
+        for index in range(len(lines)):
+            if word.lower() in lines[index].lower():
+                occur.append({
+                  "linha": index + 1,
+                  "conteudo": lines[index]
+                })
+        if len(occur):
+            result.append({
+              "palavra": word,
+              "arquivo": data["nome_do_arquivo"],
+              "ocorrencias": occur
+            })
+
+    return result
